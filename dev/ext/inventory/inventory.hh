223ab78ee3c580eeb11a6f6c7e562705e9c08147
chlewey <chlewey@gmail.com>
2017-03-29 22:48:54 -0500

    pass 1

---
+++ dev/ext/inventory/inventory.hh
@@ -0,0 +1,4 @@
+#ifndef __INVENTORY_HH__
+#define __INVENTORY_HH__
+#include "../../inc/h/interlecto.hh"
+#endif
---
+++ dev/ext/project/project.h
@@ -0,0 +1,4 @@
+#ifndef __PROJECT_HH__
+#define __PROJECT_HH__
+#include "../../inc/h/interlecto.hh"
+#endif
